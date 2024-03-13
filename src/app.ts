b507b2f59a169337746f3d996e24b55666042ddd
Guczu <jakub180720@gmail.com>
2024-03-13 19:20:11 +0100

    build(all): add cors

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,27 @@
 const express = require('express');
 import { Request, Response, Express } from "express";
 import routes from "./routers/routes";
+import cors from 'cors';
 require('dotenv').config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
+const corsOptions = {
+  origin: [
+      'http://localhost:5173'
+    ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Apple marketplace API')
 })
 
+app.use(cors(corsOptions));
 routes(app);
 
 app.listen(port, () => {
